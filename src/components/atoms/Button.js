ae50542adace3d06dbb7a6372db3825554daa0b2
Giovanna Monti <giovannamonti.ext@accurat.it>
2022-07-13 22:30:49 +0200

    Generalize button and setup correct url

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,7 +1,14 @@
-export const Button = ({ label, url, color, size }) => {
+import Link from "next/link"
+
+export const Button = ({ label, url, color, size, isSubmit }) => {
+  const localUrl = url?.replace("https://data.apesaronno.it", "")
+
   return (
-    <button className={`a-button a-button--${color} a-button--${size} link`}>
-      <a href={url}>{label}</a>
+    <button
+      type={isSubmit ? "submit" : "button"}
+      className={`a-button a-button--${color} a-button--${size} link`}
+    >
+      {!isSubmit ? <Link href={localUrl}>{label}</Link> : <a>{label}</a>}
     </button>
   )
 }
