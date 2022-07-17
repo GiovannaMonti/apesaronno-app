43dc7efc5a258f616c1231e8ba257da1a282c482
Giovanna Monti <giovannamonti.ext@accurat.it>
2022-07-17 11:34:49 +0200

    Add event archive page

--- src/pages/eventi.js
+++ src/pages/eventi.js
@@ -1,3 +1,84 @@
-export default function Eventi() {
-  return <div className="p-eventi">Eventi</div>
+import Head from "next/head"
+import { useState, useEffect } from "react"
+import { sortBy } from "lodash"
+
+import { Menu } from "../components/molecules/Menu"
+import { EventCard } from "../components/molecules/EventCard"
+import { NewsletterCard } from "../components/organisms/NewsletterCard"
+import { Footer } from "../components/organisms/Footer"
+
+import { fetchSinglePage, fetchEvents } from "../utils/fetch"
+
+export async function getStaticProps() {
+  const page = await fetchSinglePage(187)
+
+  return {
+    props: {
+      page,
+    },
+  }
+}
+
+export default function Eventi({ page }) {
+  const [upcomingEvents, setUpcomingEvents] = useState(null)
+
+  useEffect(() => {
+    ;(async function () {
+      const eventList = await fetchEvents()
+
+      const upcomingEventList = eventList
+        .filter((event) => {
+          return Date.parse(event.acf.data_evento) >= Date.parse(new Date())
+        })
+        .map((item) => ({ ...item, date: item.acf.data_evento }))
+
+      const sortedEventList = sortBy(upcomingEventList, ["date"])
+      setUpcomingEvents(sortedEventList)
+    })()
+  }, [])
+
+  const isEventsListEmpty = upcomingEvents?.length === 0
+
+  console.log("page: ", page)
+  return (
+    <>
+      <Menu />
+      <div className="p-eventi">
+        <Head>
+          <title>{page.title.rendered} | A.P.E. Saronno</title>
+          <meta
+            name="description"
+            content="Associazione proprietà edilizia. Da oltre 40 anni al fianco dei piccoli proprietari di casa."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <h1>{page.title.rendered}</h1>
+
+          <section className="o-event-section">
+            <div className="m-desc">
+              <h3>Prossimi Eventi</h3>
+              <p>
+                I nostri convegni e appuntamenti periodici per informare la
+                cittadinanza sulle principali questioni legate al mondo
+                dell’edilizia e non solo.
+              </p>
+            </div>
+            {!isEventsListEmpty && (
+              <div className="m-events">
+                {upcomingEvents?.map((event) => (
+                  <EventCard key={event.slug} event={event} />
+                ))}
+              </div>
+            )}
+          </section>
+
+          <NewsletterCard page={page} />
+        </main>
+
+        <Footer page={page} />
+      </div>
+    </>
+  )
 }
